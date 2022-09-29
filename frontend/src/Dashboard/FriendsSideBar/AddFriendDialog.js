c282b0186a0d42be54dbf3eef6797d3e79d66439
Yitong Wu <44213937+WUY97@users.noreply.github.com>
2022-09-29 01:04:16 -0700

    update material-ui api

--- frontend/src/Dashboard/FriendsSideBar/AddFriendDialog.js
+++ frontend/src/Dashboard/FriendsSideBar/AddFriendDialog.js
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from 'react'
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentTextt';
+import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { Typography } from '@mui/material';
 import { connect } from 'react-redux';
