ff78fb0c9431123d30906498e19a69d61afe9ffd
Yitong Wu <44213937+WUY97@users.noreply.github.com>
2022-09-28 23:16:45 -0700

    new commit

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const socketServer = require('./socketServer');
+const authRoutes = require('./routes/authRoutes');
+const friendInvitationRoutes = require('./routes/friendInvitationRoutes');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, '../frontend', 'build', 'index.html')
+        );
+    });
+}
+
+// register the routes
+app.use('/api/auth', authRoutes);
+app.use('/api/friend-invitation', friendInvitationRoutes);
+
+const server = http.createServer(app);
+socketServer.registerSocketServer(server);
+
+mongoose
+    .connect(process.env.REACT_APP_MONGO_URI)
+    .then(() => {
+        server.listen(PORT, () => {
+            console.log('Server is listening on ' + PORT);
+        });
+    })
+    .catch((err) => {
+        console.log('database connection failed. Sever not started');
+        console.error(err);
+    });
